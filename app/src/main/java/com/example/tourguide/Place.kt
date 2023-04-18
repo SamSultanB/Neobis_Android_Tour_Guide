bf833ae143f88b6b483ca584e90edab5ba6217a7
lux <ssbekmuratov@gmail.com>
2023-04-18 23:06:07 +0600

    fragments

---
+++ app/src/main/java/com/example/tourguide/Place.kt
@@ -0,0 +1,9 @@
+package com.example.tourguide
+
+class Place(val photo: Int,
+            val name: String,
+            val address: String,
+            val timeTable: String,
+            val distance: String,
+            val price: String,
+            val description: String)
